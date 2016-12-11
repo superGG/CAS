6d98040f178720c93ad00fa641362570ae8a5873
Chuancy <2430324843@qq.com>
2016-12-11 20:49:06 +0800

    修改common.css

--- webapp/js/activitymanage.js
+++ webapp/js/activitymanage.js
@@ -55,8 +55,7 @@ function changeActivity(id){
 	var mparm = "id="+id;
 	$.get(url, mparm, function(data){
 		if (data.serviceResult) {
-			var aData = data.resultParm.activity;
-			console.log(aData);
+			var aData = data.resultParm.activity
 			var str='<div class="upDataFrame"><div class="activity_title"><span>活动标题</span><input type="text" name="activity_title"/></div><div class="activity_content"><div class="content_lebel">活动内容</div><div id="editor"></div></div><div id="saveActivity"onclick="updataActivity('+id+')">保存</div></div>';
 			$(".cmm_content").append(str);
 			if (editor){
@@ -142,8 +141,6 @@ function getMyCreateClub() {
 		if (data.serviceResult) {
 			clubId = data.resultParm.club.id;
 			showClubActivity(1);
-		}else{
-			alert(data.ResultInfo);
 		}
 	})
 }
